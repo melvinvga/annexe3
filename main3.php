8eae62e77ef7d74e540952ac97276df4f656e71b
melvinvga <91917731+melvinvga@users.noreply.github.com>
2021-12-15 22:55:34 +0100

    Add files via upload

---
+++ main3.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Premiers tests du CSS</title>
+</head>
+
+<body>
+    <header>
+        <h1>C o m p a g n i e A t l a n t i k</h1>
+    </header>
+    <main>
+        <div class="gauche">
+            <ul>
+                <?php
+                $bdd = mysqli_connect('172.28.100.3', 'mviougea', 'elini01', 'mviougea_atlantik');
+                if (!$bdd) {
+                    die("error");
+                } else {
+                    //echo "connexion réussie";
+                }
+
+                $txtSec = "SELECT numeroSecteur,libelleSecteur FROM secteur ORDER BY libelleSecteur ASC";
+                $reqSec = mysqli_query($bdd, $txtSec);
+
+                while ($donnees = mysqli_fetch_array($reqSec)) {
+
+                ?>
+                    <li><a href="main3.php?secteur=<?= $donnees["numeroSecteur"] ?>"><?= $donnees['libelleSecteur']; ?></li></a>
+                <?php
+                }
+                ?>
+            </ul>
+        </div>
+        <div class="droite">
+            Sélectionner la liaison, et la date souhaitée<br>
+            <form action='main32.php' method='GET'>
+                <select name="libelleLiaison" id="libelleLiaison-select">
+                    <?php
+                    $numeroSec = $_GET['secteur'];
+
+                    $txtSelect = "SELECT CodeLiaison, libelleLiaison FROM liaison WHERE numerosecteur='" . $numeroSec . "'";
+                    $reqSelect = mysqli_query($bdd, $txtSelect);
+
+                    while ($donnees = mysqli_fetch_array($reqSelect)) {
+
+                    ?>
+                        <option value="<?= $donnees["libelleLiaison"] ?>"><?php echo $donnees['libelleLiaison']; ?></option>
+                    <?php
+                    }
+                    ?>
+                </select>
+                <select name="dateTraversée" id="dateTraversée-select">
+                    <?php
+                    $txtDate = "SELECT DISTINCT dateTraversée FROM traversée";
+                    $reqDate = mysqli_query($bdd, $txtDate);
+
+                    while ($donnees = mysqli_fetch_array($reqDate)) {
+
+                    ?>
+                        <option value="<?= $donnees["dateTraversée"] ?>"><?php echo $donnees['dateTraversée']; ?></option>
+                    <?php
+                    }
+                    ?>
+                </select>
+
+                <input type="submit" value="Afficher les traversées"><br>
+            </form>
+            <br>
+            <?php
+            if (isset($_GET['libelleLiaison']) && isset($_GET['dateTraversée'])) {
+                echo $_GET['libelleLiaison'] . "<br>";
+                echo "Traversées pour le " . $_GET['dateTraversée'] . ". Sélectionner la traversée souhaitée";
+            }
+            ?>
+        </div>
+    </main>
+</body>
+
+</html>
